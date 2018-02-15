168148fa5aed4ac6809a6e4f29bd0e68dbf20daa
William Durand <will+git@drnd.me>
2018-02-15 14:38:40 +0100

    Remove servicesEditor/servicesList components, use zoapp-front

--- src/shared/containers/servicesContainer.jsx
+++ src/shared/containers/servicesContainer.jsx
@@ -4,10 +4,15 @@ import { connect } from "react-redux";
 import { Cell } from "react-mdl";
 import { DialogManager } from "zoapp-ui";
 import List from "zoapp-front/components/listComponent";
-import ServicesList from "../components/servicesList";
-import displayWebServiceEditor from "../components/serviceEditor";
-import displayProviderEditor from "../components/providerEditor";
-import { apiGetMiddlewaresRequest, apiSetMiddlewareRequest, apiDeleteMiddlewareRequest } from "../actions/api";
+import ServicesList from "zoapp-front/components/servicesList";
+import displayWebServiceEditor from "zoapp-front/components/displayWebServiceEditor";
+
+import displayProviderEditor from "../components/displayProviderEditor";
+import {
+  apiGetMiddlewaresRequest,
+  apiSetMiddlewareRequest,
+  apiDeleteMiddlewareRequest,
+} from "../actions/api";
 
 const divCellStyle = {
   width: "100%",
