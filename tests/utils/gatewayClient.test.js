54efb9fc7aaec963e9885f79a8b956dbd5f208a5
Manuel Raynaud <manu@raynaud.io>
2018-04-12 09:05:38 +0200

    test GatewayClient methods

--- tests/utils/gatewayClient.test.js
+++ tests/utils/gatewayClient.test.js
@@ -4,11 +4,12 @@
  * This source code is licensed under the GPL v2.0+ license found in the
  * LICENSE file in the root directory of this source tree.
  */
+import fetch from "zoapp-backend/utils/fetch";
 import {
   getGatewayClient,
   initGatewayClient,
   GatewayClient,
-} from "../../src/utils/gatewayClient";
+} from "opla-backend/utils/gatewayClient";
 
 describe("getGatewayClient", () => {
   it("throws an error if the gateway is not initialized", () => {
@@ -21,5 +22,28 @@ describe("getGatewayClient", () => {
     initGatewayClient();
     const client = getGatewayClient();
     expect(client).toBeInstanceOf(GatewayClient);
+  });
+});
+
+jest.mock("zoapp-backend/utils/fetch");
+describe("GatewayClient", () => {
+  beforeEach(() => {
+    fetch.mockClear();
+  });
+
+  describe("getTemplates", () => {
+    it("should call /templates endpoint", () => {
+      const client = new GatewayClient("https://gateway.opla.ai");
+      client.getTemplates();
+      expect(fetch).toHaveBeenCalledWith("https://gateway.opla.ai/templates");
+    });
+  });
+
+  describe("getLanguages", () => {
+    it("should call /languages endpoint", () => {
+      const client = new GatewayClient("https://gateway.opla.ai");
+      client.getLanguages();
+      expect(fetch).toHaveBeenCalledWith("https://gateway.opla.ai/languages");
+    });
   });
 });
