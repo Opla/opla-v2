f8ae63250513e1644c0c481b296a2c02b7b68629
Manuel Raynaud <manu@raynaud.io>
2018-04-12 09:29:54 +0200

    add test form admin routes

---
+++ tests/routes/admin.test.js
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2015-present, CWB SAS
+ *
+ * This source code is licensed under the GPL v2.0+ license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import AdminRoutes from "opla-backend/routes/admin";
+
+describe("routes/admin", () => {
+  describe("getTemplates", () => {
+    it("calls the controller", () => {
+      const getTemplatesSpy = jest.fn();
+      const fakeZoapp = {
+        extensions: {
+          getAdmin: () => ({
+            getTemplates: getTemplatesSpy,
+          }),
+        },
+      };
+
+      const routes = new AdminRoutes(fakeZoapp);
+      expect(getTemplatesSpy).not.toHaveBeenCalled();
+      routes.getTemplates();
+      expect(getTemplatesSpy).toHaveBeenCalled();
+    });
+  });
+});
