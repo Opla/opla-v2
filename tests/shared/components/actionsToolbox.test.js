e2a316a88020037a1af6a4290febb45ae6e92129
williamdaconceicao <willidaconceicao@gmail.com>
2018-04-24 15:07:06 +0200

    start creating tests for actionsToolbox

---
+++ tests/shared/components/actionsToolbox.test.js
@@ -0,0 +1,36 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import ActionsToolbox from "shared/components/actionsToolbox";
+
+describe("components/actionsToolbox", () => {
+  it("can have differents behavior on change", () => {
+    const component = renderer.create(<ActionsToolbox onChange={jest.fn()} />);
+
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("can be typed", () => {
+  	const types = "any";
+    const component = renderer.create(<ActionsToolbox type={types} />);
+
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("can be an input", () => {
+  	const types = "any";
+    const component = renderer.create(<ActionsToolbox isInput />);
+
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("can be disabled", () => {
+  	const types = "any";
+    const component = renderer.create(<ActionsToolbox disable />);
+
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
