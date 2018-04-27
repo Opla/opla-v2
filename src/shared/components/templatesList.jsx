c58ce16862406172e7f925e43c9909ca899583e9
U-Craaftx-Pc\Craaftx <guilmain.dorian@gmail.com>
2018-04-27 13:35:49 +0200

    Change local style constants by css classes

--- src/shared/components/templatesList.jsx
+++ src/shared/components/templatesList.jsx
@@ -10,43 +10,6 @@ import { Grid, Inner, Cell } from "zrmc";
 import makeClassName from "classnames";
 import FileInput from "zoapp-front/components/fileInput";
 
-const infoStyleC = {
-  position: "relative",
-  fontSize: "16px",
-  fontWeight: "300",
-  color: "#FFF",
-  lineHeight: "1.6",
-  height: "180px",
-};
-
-const templateBoxStyle = {
-  position: "relative",
-  height: "250px",
-};
-
-const anchorStyle = {
-  textDecoration: "none",
-  width: "100%",
-};
-
-const imgStyle = {
-  position: "absolute",
-  margin: "0",
-  top: "50%",
-  left: "50%",
-  "-ms-transform": "translate(-50%, -50%)",
-  transform: "translate(-50%, -50%)",
-  width: "80px",
-};
-
-const imgCellStyle = {
-  textAlign: "center",
-  width: "100%",
-  height: "180px",
-  "padding-top": "30px",
-  "background-color": "#f5f5f5",
-};
-
 const TemplatesList = ({
   items,
   selectedItem,
@@ -59,44 +22,35 @@ const TemplatesList = ({
       {items.map((item, index) => {
         const key = item.id ? item.id : `${index}${item.name}`;
         const isSelectedItem = selectedItem === index;
-        const className = makeClassName("mdl-shadow--2dp", {
+        const className = makeClassName({
           selectableListItem: !isSelectedItem,
           selectedListItem: isSelectedItem,
         });
 
         return (
-          <Cell
-            key={key}
-            style={templateBoxStyle}
-            className={className}
-            span={2}
-          >
+          <Cell key={key} className="tpl-templateBox" span={2}>
             <div
               role="presentation"
-              style={anchorStyle}
+              className="tpl-anchor"
               onClick={() => {
                 onSelect(index);
               }}
             >
-              <div style={infoStyleC}>
-                <div style={imgCellStyle}>
+              <div className={`tpl-infoC ${className}`}>
+                <div className="tpl-imgCell">
                   {item.name === "Import" ? (
                     <form>
-                      <FileInput
-                        onLoad={onImport}
-                        accept={acceptImport}
-                        style={{ margin: "0px !important" }}
-                      />
+                      <FileInput onLoad={onImport} accept={acceptImport} />
                     </form>
                   ) : (
                     <img
                       src={`./images/robots/robot-${index}.svg`}
-                      style={imgStyle}
+                      className="tpl-img"
                       alt={item.name}
                     />
                   )}
                 </div>
-                <div style={{ padding: "16px", "background-color": "#455a64" }}>
+                <div className="tpl-textCell">
                   <b>{item.name}</b>
                 </div>
               </div>
