091088f7043c93c66d819925f53ff3bbf1a84305
lodelestra <loicdelestra@hotmail.fr>
2018-05-23 11:00:36 +0200

    Test IntentDetail render input intents

--- tests/shared/components/intentDetail.test.js
+++ tests/shared/components/intentDetail.test.js
@@ -5,6 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 import React from "react";
+import { shallow } from "enzyme";
 import renderer from "react-test-renderer";
 import IntentDetail from "shared/components/intentDetail";
 
@@ -23,6 +24,36 @@ describe("components/intentDetail", () => {
     );
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it("Should render input intents", () => {
+    const intent = {
+      id: "T4Zs",
+      botId: "HZAw",
+      name: "Barquette alimentaire",
+      input: ["*barquette", "barquette*", "*barquette*"],
+      output: ["Attention, les barquettes alimentaires ne peuvent..."],
+      order: 7,
+    };
+    const wrapper = shallow(<IntentDetail {...defaultProps} intent={intent} />);
+
+    expect(wrapper.find("ActionsList")).toHaveLength(2);
+    expect(
+      wrapper
+        .find("ActionsList")
+        .at(0)
+        .dive()
+        .find("ActionsEditable"),
+    ).toHaveLength(4);
+    expect(
+      wrapper
+        .find("ActionsList")
+        .at(0)
+        .dive()
+        .find("ActionsEditable")
+        .at(2)
+        .prop("content"),
+    ).toEqual(intent.input[2]);
   });
 
   it("can render intent with special characters correctly", () => {
