f436ee6cbd420d980efeb1a77e61a25d699d916e
lodelestra <loicdelestra@hotmail.fr>
2018-05-24 16:43:28 +0200

    test IntentContainer appSetIntentAction call via handleChangeAction

--- tests/shared/containers/intentContainer.test.js
+++ tests/shared/containers/intentContainer.test.js
@@ -63,4 +63,79 @@ describe("containers/IntentContainerBase", () => {
     });
     expect(resetSpy).toHaveBeenCalled();
   });
+
+  describe("handleChangeAction", () => {
+    it("should not call appSetIntentAction when new intent action doesnt have a value", () => {
+      const appSetIntentActionSpy = jest.fn();
+      const wrapper = mount(
+        <IntentContainerBase
+          {...defaultProps}
+          appSetIntentAction={appSetIntentActionSpy}
+        />,
+      );
+
+      // TODO refactor method to use params with default value instead of this.variable
+      wrapper.instance().actionContainer = "input";
+      wrapper.instance().selectedAction = undefined;
+
+      const text = undefined;
+      const name = undefined;
+      const value = undefined;
+      wrapper.instance().handleChangeAction(text, name, value);
+
+      expect(appSetIntentActionSpy).not.toHaveBeenCalled();
+    });
+
+    it("sould call appSetIntentAction when intent action is edited", () => {
+      const appSetIntentActionSpy = jest.fn();
+      const wrapper = mount(
+        <IntentContainerBase
+          {...defaultProps}
+          appSetIntentAction={appSetIntentActionSpy}
+        />,
+      );
+
+      // TODO refactor method to use params with default value instead of this.variable
+      wrapper.instance().actionContainer = "input";
+      wrapper.instance().selectedAction = 1;
+
+      const text = "* ménagers *";
+      const name = null;
+      const value = null;
+      wrapper.instance().handleChangeAction(text, name, value);
+
+      expect(appSetIntentActionSpy).toHaveBeenCalled();
+      expect(appSetIntentActionSpy.mock.calls[0][0]).toEqual("input");
+      expect(appSetIntentActionSpy.mock.calls[0][1]).toEqual(undefined);
+      expect(appSetIntentActionSpy.mock.calls[0][2]).toEqual(text);
+      expect(appSetIntentActionSpy.mock.calls[0][3]).toBe(1);
+    });
+
+    it("should call appSetIntentAction when new intent action saved", () => {
+      const appSetIntentActionSpy = jest.fn();
+      const wrapper = mount(
+        <IntentContainerBase
+          {...defaultProps}
+          appSetIntentAction={appSetIntentActionSpy}
+        />,
+      );
+
+      // TODO refactor method to use params with default value instead of this.variable
+      wrapper.instance().actionContainer = "input";
+      wrapper.instance().selectedAction = undefined;
+
+      const text = "* new action *";
+      const name = undefined;
+      const value = undefined;
+      wrapper.instance().handleChangeAction(text, name, value);
+
+      expect(appSetIntentActionSpy).toHaveBeenCalled();
+      expect(appSetIntentActionSpy.mock.calls[0][0]).toEqual("input");
+      expect(appSetIntentActionSpy.mock.calls[0][1]).toEqual(undefined);
+      expect(appSetIntentActionSpy.mock.calls[0][2]).toEqual(text);
+      expect(appSetIntentActionSpy.mock.calls[0][3]).toBe(undefined);
+    });
+
+    // TODO find the case where this.actionType === "condition" and test it
+  });
 });
