de90eb4d7c7519cb7dbf813d39841ee6065ca650
lodelestra <loicdelestra@hotmail.fr>
2018-05-24 22:51:38 +0200

    test IntentContainer handleActions parameters

--- tests/shared/containers/intentContainer.test.js
+++ tests/shared/containers/intentContainer.test.js
@@ -9,6 +9,8 @@ import { mount } from "enzyme";
 
 import { IntentContainerBase } from "shared/containers/intentContainer";
 import ActionsEditable from "shared/components/actionsEditable";
+import * as intentDetail from "shared/components/intentDetail";
+import Zrmc from "zrmc";
 
 describe("containers/IntentContainerBase", () => {
   const defaultProps = {
@@ -137,5 +139,293 @@ describe("containers/IntentContainerBase", () => {
     });
 
     // TODO find the case where this.actionType === "condition" and test it
+  });
+
+  describe("handleActions", () => {
+    describe("on intentDetail item selected", () => {
+      const args = {
+        name: "input",
+        type: undefined,
+        state: "select",
+        index: 1,
+      };
+
+      it("should set component fields and state", () => {
+        const wrapper = mount(<IntentContainerBase {...defaultProps} />);
+        wrapper.update();
+        expect(wrapper.find(ActionsEditable)).toHaveLength(8);
+
+        // set this.actionContainer
+        wrapper.instance().actionContainer = undefined;
+
+        wrapper.instance().handleActions(args);
+
+        // should set this.actionContainer
+        // should set this.actionType
+        // should set this.selectedAction
+        expect(wrapper.instance().actionContainer).toEqual(args.name);
+        expect(wrapper.instance().actionType).toEqual(args.type);
+        expect(wrapper.instance().selectedAction).toEqual(args.index);
+
+        // can have diff parameters if type === 'condition'
+
+        // should set editing state
+        expect(wrapper.state().editing).toBe(true);
+      });
+
+      it("should call displayActionEditor", () => {
+        // mock displayActionEditor function
+        const displayActionEditorSpy = jest.fn();
+        // eslint-disable-next-line import/namespace
+        intentDetail.displayActionEditor = displayActionEditorSpy;
+        const wrapper = mount(<IntentContainerBase {...defaultProps} />);
+        wrapper.update();
+        expect(wrapper.find(ActionsEditable)).toHaveLength(8);
+
+        // set this.actionContainer
+        wrapper.instance().actionContainer = undefined;
+
+        wrapper.instance().handleActions(args);
+
+        expect(displayActionEditorSpy).toHaveBeenCalled();
+        const expectedActionEditorArgs = {
+          title: "Edit input item",
+          type: undefined,
+          action: "Change",
+          actionDef: "Change",
+          parameters: "* Produits ménagers *",
+          function: () => {},
+          handleEditAction: () => {},
+          handleChangeAction: () => {},
+          isInput: true,
+        };
+        expect(displayActionEditorSpy.mock.calls[0][0]).toEqual(
+          expectedActionEditorArgs.title,
+        );
+        expect(displayActionEditorSpy.mock.calls[0][1]).toEqual(
+          expectedActionEditorArgs.type,
+        );
+        expect(displayActionEditorSpy.mock.calls[0][2]).toEqual(
+          expectedActionEditorArgs.action,
+        );
+        expect(displayActionEditorSpy.mock.calls[0][3]).toEqual(
+          expectedActionEditorArgs.actionDef,
+        );
+        expect(displayActionEditorSpy.mock.calls[0][4]).toEqual(
+          expectedActionEditorArgs.parameters,
+        );
+        expect(displayActionEditorSpy.mock.calls[0][8]).toEqual(
+          expectedActionEditorArgs.isInput,
+        );
+      });
+    });
+
+    describe("on intentDetail item delete", () => {
+      const args = {
+        name: "input",
+        type: undefined,
+        state: "delete",
+        index: 2,
+      };
+
+      it("should set component fields and state", () => {
+        const wrapper = mount(<IntentContainerBase {...defaultProps} />);
+        wrapper.update();
+        expect(wrapper.find(ActionsEditable)).toHaveLength(8);
+
+        // set this.actionContainer
+        wrapper.instance().actionContainer = undefined;
+
+        wrapper.instance().handleActions(args);
+
+        // should set this.actionContainer
+        // should set this.actionType
+        // should set this.selectedAction
+        expect(wrapper.instance().actionContainer).toEqual(args.name);
+        expect(wrapper.instance().actionType).toEqual(args.type);
+        expect(wrapper.instance().selectedAction).toEqual(args.index);
+
+        // can have diff parameters if type === 'condition'
+
+        // should set editing state
+        expect(wrapper.state().editing).toBe(false);
+      });
+
+      it("should call Zrmc.showDialog", () => {
+        const showDialogSpy = jest.fn();
+        Zrmc.showDialog = showDialogSpy;
+        const wrapper = mount(<IntentContainerBase {...defaultProps} />);
+        wrapper.update();
+        expect(wrapper.find(ActionsEditable)).toHaveLength(8);
+
+        // set this.actionContainer
+        wrapper.instance().actionContainer = undefined;
+
+        wrapper.instance().handleActions(args);
+
+        expect(showDialogSpy).toHaveBeenCalled();
+        expect(showDialogSpy.mock.calls[0][0].actions[1].name).toEqual(
+          "Delete",
+        );
+      });
+    });
+
+    describe("on intentDetail parameters topic selected", () => {
+      const args = {
+        // name: "Emballages produits ménagers",
+        name: defaultProps.selectedIntent.name,
+        type: undefined,
+        state: "topic",
+        index: undefined,
+      };
+
+      it("should set component fields and state", () => {
+        const wrapper = mount(<IntentContainerBase {...defaultProps} />);
+        wrapper.update();
+        expect(wrapper.find(ActionsEditable)).toHaveLength(8);
+
+        // set this.actionContainer
+        wrapper.instance().actionContainer = undefined;
+
+        wrapper.instance().handleActions(args);
+
+        // should set this.actionContainer
+        // should set this.actionType
+        // should set this.selectedAction
+        expect(wrapper.instance().actionContainer).toEqual(args.name);
+        expect(wrapper.instance().actionType).toEqual(args.type);
+        expect(wrapper.instance().selectedAction).toEqual(args.index);
+
+        // can have diff parameters if type === 'condition'
+
+        // should set editing state
+        expect(wrapper.state().editing).toBe(true);
+      });
+
+      it("should call displayActionEditor", () => {
+        // mock displayActionEditor function
+        const displayActionEditorSpy = jest.fn();
+        // eslint-disable-next-line import/namespace
+        intentDetail.displayActionEditor = displayActionEditorSpy;
+        const wrapper = mount(<IntentContainerBase {...defaultProps} />);
+        wrapper.update();
+        expect(wrapper.find(ActionsEditable)).toHaveLength(8);
+
+        // set this.actionContainer
+        wrapper.instance().actionContainer = undefined;
+
+        wrapper.instance().handleActions(args);
+
+        expect(displayActionEditorSpy).toHaveBeenCalled();
+        const expectedActionEditorArgs = {
+          title: "Set topic name",
+          type: undefined,
+          action: "Set",
+          actionDef: "Topic",
+          parameters: "*",
+          function: () => {},
+          handleEditAction: () => {},
+          handleChangeAction: () => {},
+          isInput: false,
+        };
+        expect(displayActionEditorSpy.mock.calls[0][0]).toEqual(
+          expectedActionEditorArgs.title,
+        );
+        expect(displayActionEditorSpy.mock.calls[0][1]).toEqual(
+          expectedActionEditorArgs.type,
+        );
+        expect(displayActionEditorSpy.mock.calls[0][2]).toEqual(
+          expectedActionEditorArgs.action,
+        );
+        expect(displayActionEditorSpy.mock.calls[0][3]).toEqual(
+          expectedActionEditorArgs.actionDef,
+        );
+        expect(displayActionEditorSpy.mock.calls[0][4]).toEqual(
+          expectedActionEditorArgs.parameters,
+        );
+        expect(displayActionEditorSpy.mock.calls[0][8]).toEqual(
+          expectedActionEditorArgs.isInput,
+        );
+      });
+    });
+
+    describe("on intentDetail parameters Previous selected", () => {
+      const args = {
+        // name: "Emballages produits ménagers",
+        name: defaultProps.selectedIntent.name,
+        type: undefined,
+        state: "previous",
+        index: undefined,
+      };
+
+      it("should set component fields and state", () => {
+        const wrapper = mount(<IntentContainerBase {...defaultProps} />);
+        wrapper.update();
+        expect(wrapper.find(ActionsEditable)).toHaveLength(8);
+
+        // set this.actionContainer
+        wrapper.instance().actionContainer = undefined;
+
+        wrapper.instance().handleActions(args);
+
+        // should set this.actionContainer
+        // should set this.actionType
+        // should set this.selectedAction
+        expect(wrapper.instance().actionContainer).toEqual(args.name);
+        expect(wrapper.instance().actionType).toEqual(args.type);
+        expect(wrapper.instance().selectedAction).toEqual(args.index);
+
+        // can have diff parameters if type === 'condition'
+
+        // should set editing state
+        expect(wrapper.state().editing).toBe(true);
+      });
+
+      it("should call displayActionEditor", () => {
+        // mock displayActionEditor function
+        const displayActionEditorSpy = jest.fn();
+        // eslint-disable-next-line import/namespace
+        intentDetail.displayActionEditor = displayActionEditorSpy;
+        const wrapper = mount(<IntentContainerBase {...defaultProps} />);
+        wrapper.update();
+        expect(wrapper.find(ActionsEditable)).toHaveLength(8);
+
+        // set this.actionContainer
+        wrapper.instance().actionContainer = undefined;
+
+        wrapper.instance().handleActions(args);
+
+        expect(displayActionEditorSpy).toHaveBeenCalled();
+        const expectedActionEditorArgs = {
+          title: "Set previous intent",
+          type: undefined,
+          action: "Set",
+          actionDef: "Previous",
+          parameters: undefined,
+          function: () => {},
+          handleEditAction: () => {},
+          handleChangeAction: () => {},
+          isInput: false,
+        };
+        expect(displayActionEditorSpy.mock.calls[0][0]).toEqual(
+          expectedActionEditorArgs.title,
+        );
+        expect(displayActionEditorSpy.mock.calls[0][1]).toEqual(
+          expectedActionEditorArgs.type,
+        );
+        expect(displayActionEditorSpy.mock.calls[0][2]).toEqual(
+          expectedActionEditorArgs.action,
+        );
+        expect(displayActionEditorSpy.mock.calls[0][3]).toEqual(
+          expectedActionEditorArgs.actionDef,
+        );
+        expect(displayActionEditorSpy.mock.calls[0][4]).toEqual(
+          expectedActionEditorArgs.parameters,
+        );
+        expect(displayActionEditorSpy.mock.calls[0][8]).toEqual(
+          expectedActionEditorArgs.isInput,
+        );
+      });
+    });
   });
 });
