554c50a22d0dda21e7ce43a754606ac0d837df61
lodelestra <loicdelestra@hotmail.fr>
2018-06-08 12:45:03 +0200

    test new components

---
+++ tests/shared/components/conditionsActionsEditable.test.js
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2015-present, CWB SAS
+ *
+ * This source code is licensed under the GPL v2.0+ license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import React from "react";
+import { shallow } from "enzyme";
+import ConditionsActionsEditable from "shared/components/conditionsActionsEditable";
+
+describe("components/actionsItem", () => {
+  const defaultContainerName = "output";
+  const defaultAction = {
+    name: "actionName",
+    value: "actionValue",
+    text: "action text",
+  };
+
+  it("should render with actions conditions", () => {
+    const wrapper = shallow(
+      <ConditionsActionsEditable
+        containerName={defaultContainerName}
+        content={defaultAction}
+      />,
+    );
+
+    expect(wrapper.find("TextField")).toHaveLength(2);
+    expect(wrapper.find("#conditionName")).toHaveLength(1);
+    expect(wrapper.find("#conditionName").prop("value")).toEqual("actionName");
+    expect(wrapper.find("#conditionValue")).toHaveLength(1);
+    expect(wrapper.find("#conditionValue").prop("value")).toEqual(
+      "actionValue",
+    );
+
+    expect(wrapper.find("ActionsEditable")).toHaveLength(1);
+    expect(wrapper.find("ActionsEditable").prop("content")).toEqual(
+      "action text",
+    );
+  });
+});
