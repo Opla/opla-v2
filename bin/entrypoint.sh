c2a3f880e86adc13bf8e552103d3ed99a024e6e0
Thomas Carli <thomas.carli@gmail.com>
2018-06-19 14:38:28 +0200

    publish chart

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -2,6 +2,15 @@
 
 # If there is an existing configuration
 # given as a volume, just take it.
+
+waitForDbConnection() {
+	until nc -z -v -w30 $OPLA_BACKEND_DATABASE_HOST $OPLA_BACKEND_DATABASE_PORT; do
+		echo "Waiting for database connection..."
+		# wait for 2 seconds before check again
+		sleep 1
+	done
+}
+
 if [ -f config/config.json ]; then
 	mv config/config.json config/default.json
 else
@@ -26,7 +35,14 @@ if [ -n "$SKIP_MIGRATION" -a "$SKIP_MIGRATION" = 'true' ]; then
 	echo "Skipping migrations."
 
 else
+	waitForDbConnection
 	./bin/opla migrations up --non-interactive
 fi
 
-exec node --inspect=0.0.0.0:9229 dist/
+if [ -n "$MIGRATIONS_ONLY" -a "$MIGRATIONS_ONLY" = 'true' ]; then
+	echo "MIGRATIONS_ONLY. Not executing node."
+else
+	waitForDbConnection
+	exec node --inspect=0.0.0.0:9229 dist/
+fi
+
