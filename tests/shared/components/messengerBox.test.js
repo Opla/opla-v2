b214fad2b29e7135be253cdc25f8d1d26cd47992
mikbry <mik@opla.ai>
2018-07-04 13:24:52 +0200

    Fix tests

--- tests/shared/components/messengerBox.test.js
+++ tests/shared/components/messengerBox.test.js
@@ -61,10 +61,10 @@ describe("components/messengerBox", () => {
     expect(wrapper.find(".message")).toHaveLength(4);
     const content = wrapper.find(".message").map((node) => node.text());
     expect(content).toEqual([
-      "msg1#Intent.output.0",
-      "rep1#Intent.output.0",
-      "msg2#Intent.output.0",
-      "rep2#Intent.output.0",
+      "msg1<Icon />#NotFoundIntent..output",
+      "rep1<Icon />#NotFoundIntent..output",
+      "msg2<Icon />#NotFoundIntent..output",
+      "rep2<Icon />#NotFoundIntent..output",
     ]);
   });
 
@@ -82,7 +82,7 @@ describe("components/messengerBox", () => {
     );
     expect(wrapper.find(".message")).toHaveLength(1);
     expect(wrapper.find(".message").text()).toEqual(
-      "12/09/2018#Intent.output.0",
+      "12/09/2018<Icon />#NotFoundIntent..output",
     );
   });
 
@@ -100,7 +100,7 @@ describe("components/messengerBox", () => {
     );
     expect(wrapper.find(".text-wrapper")).toHaveLength(1);
     expect(wrapper.find(".text-wrapper").text()).toMatch(
-      "12/09/2018 <Button /> ou  <Button />  #Intent.output.0",
+      "12/09/2018 <Button /> ou  <Button />  <Icon />#NotFoundIntent..output",
     );
     expect(wrapper.find(Button)).toHaveLength(2);
   });
