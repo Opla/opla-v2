7659299568050eb0f4693d70a385cdfb971b5ef6
lodelestra <loicdelestra@hotmail.fr>
2018-09-19 12:16:47 +0200

    Split messengerBox components

---
+++ front/src/shared/components/messengerBox/messengerBoxFooter.jsx
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2015-present, CWB SAS
+ *
+ * This source code is licensed under the GPL v2.0+ license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import React from "react";
+
+const MessengerBoxFooter = () => (
+  <div
+    style={{
+      height: "18px",
+      textAlign: "right",
+      paddingTop: "6px",
+      paddingRight: "16px",
+      fontSize: "10px",
+      background: "white",
+    }}
+  >
+    <a
+      href="https://opla.ai"
+      style={{ color: "#aaa" }}
+      target="_blank"
+      rel="noreferrer noopener"
+    >
+      Powered by Opla.ai
+    </a>
+  </div>
+);
+export default MessengerBoxFooter;
