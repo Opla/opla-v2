4017f475402a07fa1823baa26bd38b4438764acd
Arnaud MONCEL <arnaud.moncel43@gmail.com>
2019-01-22 11:05:59 +0100

    Fix Uglify plugin deprecation on webpack
    Remove UglifyJsPlugin
    Add new recomended Terser plugin

--- front/webpack.prod.js
+++ front/webpack.prod.js
@@ -1,7 +1,7 @@
 /* eslint import/no-extraneous-dependencies: 0 */
 const merge = require("webpack-merge");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const webpack = require("webpack");
+const TerserPlugin = require("terser-webpack-plugin");
 
 const commonConfig = require("./webpack.common.js");
 
@@ -11,37 +11,27 @@ module.exports = merge(commonConfig, {
   entry: {
     app: "./client/index.jsx",
   },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          ecma: 8,
+          safari10: true,
+          output: {
+            comments: false,
+            // Turned on because emoji and regex is not minified properly using default
+            // https://github.com/facebook/create-react-app/issues/2488
+            ascii_only: true,
+          },
+        },
+        parallel: true,
+        cache: true,
+      }),
+    ],
+  },
   plugins: [
     new ExtractTextPlugin({
       filename: "css/[name].css",
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      // From: https://github.com/facebook/create-react-app/
-      uglifyOptions: {
-        ecma: 8,
-        compress: {
-          warnings: false,
-          // Disabled because of an issue with Uglify breaking seemingly valid code:
-          // https://github.com/facebook/create-react-app/issues/2376
-          // Pending further investigation:
-          // https://github.com/mishoo/UglifyJS2/issues/2011
-          comparisons: false,
-        },
-        mangle: {
-          safari10: true,
-        },
-        output: {
-          comments: false,
-          // Turned on because emoji and regex is not minified properly using default
-          // https://github.com/facebook/create-react-app/issues/2488
-          ascii_only: true,
-        },
-      },
-      // Use multi-process parallel running to improve the build speed
-      // Default number of concurrent runs: os.cpus().length - 1
-      parallel: true,
-      // Enable file caching
-      cache: true,
     }),
   ],
 });
