e5f4347bbee980b2758f5b0e246295fb583dce68
Arnaud MONCEL <arnaud.moncel43@gmail.com>
2019-02-07 15:32:22 +0100

    Remove postDeploy script

--- backend/charts/opla-backend/scripts/add_user.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -x
-
-STATUS_CODE=$(curl -o /dev/null -s -w "%{http_code}\n" 'http://backend:80/auth/user' -H 'content-type: application/json' -H 'accept: application/json' --data-binary '{"client_id": "{{.Values.db.auth.clientId}}","email":"{{.Values.default_user.email}}","username":"{{.Values.default_user.username}}","password":"{{.Values.default_user.password}}","accept": "true"}' --compressed)
-
-if [ "$STATUS_CODE" != 200 ]; then
-exit 1
-fi;
