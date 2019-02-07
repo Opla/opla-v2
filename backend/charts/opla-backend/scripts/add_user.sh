3ee81bf692996c80992427b370f0d6cc6b0bdebf
Arnaud MONCEL <arnaud.moncel43@gmail.com>
2019-02-07 15:11:16 +0100

    Should fix e2e backend

--- backend/charts/opla-backend/scripts/add_user.sh
+++ backend/charts/opla-backend/scripts/add_user.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -x
 
-STATUS_CODE=$(curl -o /dev/null -s -w "%{http_code}\n" 'http://backend:80/api/v1/bots' -H 'content-type: application/json' -H 'accept: application/json' -H 'client_id: {{.Values.db.auth.clientId}}' --data-binary '{"name":"{{.Values.default_user.bot_name}}","email":"{{.Values.default_user.email}}","username":"{{.Values.default_user.username}}","password":"{{.Values.default_user.password}}","template":{{.Values.default_user.template}},"language":null}' --compressed)
+STATUS_CODE=$(curl -o /dev/null -s -w "%{http_code}\n" 'http://backend:80/auth/user' -H 'content-type: application/json' -H 'accept: application/json' --data-binary '{"client_id": "{{.Values.db.auth.clientId}}","email":"{{.Values.default_user.email}}","username":"{{.Values.default_user.username}}","password":"{{.Values.default_user.password}}","accept": "true"}' --compressed)
 
 if [ "$STATUS_CODE" != 200 ]; then
 exit 1
