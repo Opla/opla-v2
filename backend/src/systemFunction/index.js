d789f78346bde80510536a3db15864a21b4b5e9c
Jeff LADIRAY <ladirayjeff@gmail.com>
2019-03-08 14:50:07 +0100

    Chores - add header on backend file

--- backend/src/systemFunction/index.js
+++ backend/src/systemFunction/index.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2015-present, CWB SAS
+ *
+ * This source code is licensed under the GPL v2.0+ license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import SendMail from "./sendMail";
 import GetDate from "./getDate";
 
